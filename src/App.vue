ac7e1af7fa03a8e220d687887db7bdae6838363f
“IU-zhian” <1592525832@qq.com>
2024-01-08 14:03:01 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@
   <!-- 一级路由出口组件 -->
   <!-- RouterView是<router-view></router-view>的简写 -->
   <RouterView />
+  <div class="test">
+    test scss 
+  </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .test{
+    color:$priceColor
+  }
 </style>
