a3e81b3f534018d94fb3a19a12045e3d5a8f6f74
“IU-zhian” <1592525832@qq.com>
2024-01-09 20:37:48 +0800

    home-整体结构拆分以及分类的数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,15 +1,21 @@
 <template>
-    <div style="height: 1200px;">
-
-    </div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
   
-  <script>
-  export default {
-  
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+<script setup>
+  import HomeCategory from './components/HomeCategory.vue'
+  import HomeBanner from './components/HomeBanner.vue'
+  import HomeNew from './components/HomeNew.vue'
+  import HomeHot from './components/HomeHot.vue'
+  import homeProduct from './components/HomeProduct.vue'
+</script>
+
+<style>
+
+</style>
